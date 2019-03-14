34a6ef0477b5d5e1afb94a5a0060ef819b590cdd
xiaoyuer <3445959699@qq.com>
2019-03-14 18:14:57 +0800

    添加本地工程到git

---
+++ git_test/src/com/itheima/gitTest.java
@@ -0,0 +1,15 @@
+package com.itheima;
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello!!!! Git");
+        System.out.println("hello!!!! Git");
+        System.out.println("hello!!!! Git");
+        System.out.println("hello!!!! Git");
+        System.out.println("hello!!!! Git");
+        System.out.println("hello!!!! Git");
+        System.out.println("hello!!!! Git");
+
+    }
+
+}
