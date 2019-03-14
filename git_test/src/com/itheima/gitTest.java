8710d6ce426a21f397d7f738950f44e2beb18a4c
xiaoyuer <3445959699@qq.com>
2019-03-14 18:31:07 +0800

    添加备注克隆

--- git_test/src/com/itheima/gitTest.java
+++ git_test/src/com/itheima/gitTest.java
@@ -9,7 +9,14 @@ public class gitTest {
         System.out.println("hello!!!! Git");
         System.out.println("hello!!!! Git");
         System.out.println("hello!!!! Git");
+//hello Git
+    /*
 
+
+        克隆项目
+
+
+        */
     }
 
     public  void tser(){
